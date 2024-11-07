90ca7e9dd353b689934f697e9d719588c63fe576
Mike <mike_liu@evervictory-it.com>
2024-11-07 14:58:10 +0800

    新增 SCSS 變數與樣式，更新 Nuxt 配置以包含樣式，並在首頁添加按鈕

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Page: 首頁
-  </div>
+  <div class="title">Page: index</div>
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+
+  <button type="button" class="btn btn-link">Link</button>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.title {
+  color: $main-color;
+}
+</style>
