a285ff90b4108e02baa7eea479d1317bc746cfe6
Mike <mike_liu@evervictory-it.com>
2024-11-08 13:58:45 +0800

    新增 useHome 組件以從 API 獲取新聞列表，並在首頁顯示加載狀態

--- composables/useHome.js
+++ composables/useHome.js
@@ -1,3 +1,22 @@
 export const useHome = () => {
-  return {};
+  const newsList = ref([]);
+  const isLoading = ref(false);
+
+  onMounted(async () => {
+    isLoading.value = true;
+    try {
+      const response = await fetch(
+        "https://nuxr3.zeabur.app/api/v1/home/news/"
+      );
+      const data = await response.json();
+      newsList.value = data.result;
+      console.log(newsList.value);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      isLoading.value = false;
+    }
+  });
+
+  return { isLoading, newsList };
 };
--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,26 @@
 import Loading from "vue-loading-overlay";
 import "vue-loading-overlay/dist/css/index.css";
 
-const newsList = ref([]);
-const isLoading = ref(false);
+const { isLoading, newsList } = useHome();
 
 // API 路徑 : https://nuxr3.zeabur.app/api/v1/home/news/
 // 使用 ES6 fetch() 或是 axios.get() 串接 API
 // 切換 isLoading 狀態
-
-
 </script>
 
 <template>
   <div class="container">
     <h1>最新消息</h1>
-    <!-- <NewsCard v-for="..." :key="..."  v-bind="..." /> -->
-
+    <NewsCard
+      v-for="item in newsList"
+      :key="item._id"
+      :_id="item._id"
+      :title="item.title"
+      :image="item.image"
+      :description="item.description"
+      :createdAt="item.createdAt"
+      :updatedAt="item.updatedAt"
+    />
     <ClientOnly>
       <Loading v-model:active="isLoading" />
     </ClientOnly>
