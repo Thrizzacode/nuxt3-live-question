c8725aa44d2a9942240e7a6b61ce1f717760ffad
Mike <mike_liu@evervictory-it.com>
2024-11-21 10:02:43 +0800

    更新登入功能，新增 API 串接及錯誤處理，並使用 sweetAlert2 顯示登入結果

--- pages/login.vue
+++ pages/login.vue
@@ -7,20 +7,41 @@ const userLoginObject = ref({
   password: "",
 });
 
-const loginAccount = () => {
+const loginAccount = async (body) => {
   /*
   1. 串接旅館的 登入 API
   2. 登入成功後，使用 useCookie() 將 token 寫入名稱為 “auth” 的 cookie
   3. 需使用 try catch 處理請求
   4. 請求成功與失敗皆使用 sweetAlert2 套件顯示訊息
- $swal.fire({
-   position: "center",
-   icon: ...,
-   title: ...,
-   showConfirmButton: false,
-   timer: 1500,
- });
   */
+  try {
+    const { status, token } = await $fetch("/v1/user/login", {
+      baseURL: "https://nuxr3.zeabur.app/api",
+      method: "POST",
+      body: {
+        ...body,
+      },
+    });
+    const auth = useCookie("auth", { maxAge: 600 });
+    auth.value = token;
+
+    $swal.fire({
+      position: "center",
+      icon: "success",
+      title: "登入成功",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } catch (error) {
+    const { message } = error.response._data;
+    $swal.fire({
+      position: "center",
+      icon: "error",
+      title: message,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
 };
 </script>
 
@@ -30,9 +51,10 @@ const loginAccount = () => {
       <div class="row justify-content-md-center">
         <div class="col-12 col-md-11 col-lg-8 col-xl-7 col-xxl-6">
           <h2 class="h3 mb-4">登入</h2>
-          <form>
+          <form @submit.prevent="loginAccount(userLoginObject)">
             <div class="form-floating mb-4">
               <input
+                v-model="userLoginObject.email"
                 type="email"
                 class="form-control"
                 id="email"
@@ -45,6 +67,7 @@ const loginAccount = () => {
 
             <div class="form-floating mb-4">
               <input
+                v-model="userLoginObject.password"
                 type="password"
                 class="form-control"
                 id="password"
