fb90d603bd46a9c19f6fdfe65f444fba24675e92
Mike <mike_liu@evervictory-it.com>
2024-11-26 10:01:54 +0800

    新增文本格式化和時間格式化指令，更新首頁顯示內容

--- plugins/timeformat.js
+++ plugins/timeformat.js
@@ -1,1 +1,7 @@
-export default defineNuxtPlugin((nuxtApp) => {});
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive("timeformat", (el, binding) => {
+    // el.innerHTML = binding.value.toLocaleTimeString();
+    //轉換成yyyy-mm-dd hh:mm:ss格式
+    el.innerHTML = new Date(binding.value).toLocaleString();
+  });
+});
