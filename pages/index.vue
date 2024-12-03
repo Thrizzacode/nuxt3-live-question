772c8edc681f2303a9d53d9cdd1f0564fa4c592d
Mike <mike_liu@evervictory-it.com>
2024-12-03 14:05:42 +0800

    新增 Swiper 和 SwiperSlide 組件，並在首頁整合使用以顯示房間圖片

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
 const { data: rooms } = await useFetch("/api/v1/rooms", {
   baseURL: "https://nuxr3.zeabur.app",
 });
@@ -14,9 +16,32 @@ const { data: rooms } = await useFetch("/api/v1/rooms", {
       >
         <div class="row">
           <div class="col-lg-6">
-            <pre>
-              {{ room.imageUrlList }} 
-            </pre>
+            <Swiper
+              class="h-100"
+              :modules="[Navigation, Pagination, Autoplay]"
+              :navigation="{
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+              }"
+              :pagination="{
+                el: '.swiper-pagination',
+                type: 'bullets',
+                clickable: true,
+              }"
+              :autoplay="{ delay: 5000 }"
+            >
+              <SwiperSlide
+                v-for="(image, index) in room.imageUrlList"
+                :key="index"
+              >
+                <img
+                  :src="image"
+                  class="img-fluid"
+                  alt="room image"
+                  loading="lazy"
+                />
+              </SwiperSlide>
+            </Swiper>
           </div>
           <div class="col-lg-6">
             <div class="card-body">
